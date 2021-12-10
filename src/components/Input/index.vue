296f5cfede7bbeb0aa54046c0aed4cf83557bd83
Michealchan <michaelchankawhi@gmail.com>
2021-12-10 18:04:43 +0800

    11

---
+++ src/components/Input/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-input :value="value"></el-input>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number]
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
