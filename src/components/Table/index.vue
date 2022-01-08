ed175b42ae681978625c9fcd75212bfe9bde8381
Michealchan <michaelchankawhi@gmail.com>
2022-01-08 20:10:07 +0800

    提交table

---
+++ src/components/Table/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-table v-bind="$attrs" v-on="$listeners">
+      <template v-for="item in columns">
+        <el-table-column v-bind="item" :key="item.prop" :align="item.align || 'left'"></el-table-column>
+      </template>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OrsdTable',
+  props: {
+    columns: {
+      type: Array,
+      default: () => []
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
